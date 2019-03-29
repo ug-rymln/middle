69c4bebab43478dfe9f93f3cf096efbc3846f283
ug-rymln <ug.rymln@gmail.com>
2019-03-29 15:50:03 +0800

    '0329'

--- cli3/src/views/sass.vue
+++ cli3/src/views/sass.vue
@@ -15,6 +15,23 @@
     <p class="variable fs12">
       变量 以$开头 添加!global为全局作用域
     </p>
+    <p class="operations">
+      运算
+    </p>
+    <p class="defaultc">
+      !default关键字默认赋值
+    </p>
+    <p class="extend_font green">
+      @extend .extend
+    </p>
+    <p class="mixin">
+      mixin混合指令<br>
+      @mixin可以用 = 表示<br>
+      @include可以用 + 表示
+    </p>
+    <p class="function">
+      函数指令 @function 
+    </p>
   </div>
 </template>
 
@@ -77,5 +94,64 @@ export default {
   .fs12 {
     font-size: $p12;
   }
+
+  //运算
+  .operations {
+    $height: 80px;
+    width: (500px / 2);
+    height: $height / 2;
+    // margin-left: 4px + (10px / 2);
+    margin: 4px + 2px auto;
+    $font-size: 14px;
+    $line-height: 24px;
+    font: #{$font-size}/#{$line-height} PingFang SC;
+    color: #020203 + #7d83d3;
+  }
+
+  //默认赋值
+  .defaultc {
+    $content: '这是默认的值' !default;
+    content: $content;
+  }
   
+  //延伸
+  .extend {
+    color: blue;
+    font-size: 20px;
+    font-style: italic;
+  }
+  .extend.green {
+    color: green;
+  }
+  .extend_font {
+    @extend .extend;
+    color: red;
+  }
+
+  //mixin混合
+  @mixin large-text($px, $color, $width: 200px) {
+    font: {
+      family: Arial;
+      size: $px;
+      weight: bold;
+    }
+    color: $color;
+    width: $width;
+  }
+  .mixin {
+    @include large-text(14px,#ff0000);
+  }
+
+  //...
+
+  //函数指令
+  $grid-width: 40px;
+  $gutter-width: 10px;
+  @function grid-width($n) {
+    @return $n * $grid-width + ($n - 1) * $gutter-width;
+  }
+  .function {
+    width: grid-width(5);
+  } 
+
 </style>
